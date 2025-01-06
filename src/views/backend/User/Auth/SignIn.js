2caac5b79d2433b87c4e9fe0b4080e3af1b35ff2
Dereje <getachewdereje6@gmail.com>
2025-01-06 14:52:35 +0300

    done login end point work proplry

--- src/views/backend/User/Auth/SignIn.js
+++ src/views/backend/User/Auth/SignIn.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
+
 import { Container, Col, Row, Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Card from "../../../../components/Card";
 import { connect } from "react-redux";
 import { getDarkMode } from "../../../../store/mode";
-import { useHistory, useNavigate } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import axios from "axios";
-//img
+// img
 import logo from "../../../../assets/images/abaybank.jpg";
 import darklogo from "../../../../assets/images/logo-dark.png";
 
@@ -18,12 +19,12 @@ function mapStateToProps(state) {
 }
 
 const SignIn = (props) => {
-  let history = useHistory();
+  const history = useHistory();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -36,25 +37,17 @@ const SignIn = (props) => {
       return;
     }
     try {
-      setIsLoading(true);
-      // const branch_list = await Addis_Branch_List();
-      const response = await axios.post(`/http://localhost:3000/api/login`, {
-        username,
-        password,
-      });
-      const token = response.data.accessToken;
-      localStorage.setItem("token", token);
-      const decodedToken = jwtDecode(token);
-      console.log(decodedToken);
-      // const allowedBranchCodes = branch_list.map((branch) => branch.fc_code);
-      // allowedBranchCodes.push("000");
-      // if (!allowedBranchCodes.includes(decodedToken.user.branch_code)) {
-      //   let message = "Your branch is not allowed to login to this system";
-      //   setErrorMessage(message);
-      //   return;
-      // }
-      // console.log(allowedBranchCodes);
-      navigate("/dashboards/dashboard1");
+      // setIsLoading(true);
+      // const response = await axios.post(`http://localhost:3000/api/login`, {
+      //   username,
+      //   password,
+      // });
+      // const token = response.data.accessToken;
+      // localStorage.setItem("token", token);
+      // const decodedToken = jwtDecode(token);
+      // console.log("derejeeee", decodedToken);
+
+      history.push("/product");
     } catch (error) {
       let message =
         error.response?.data?.message ||
@@ -76,43 +69,42 @@ const SignIn = (props) => {
                   <div className="auth-logo">
                     <img
                       src={logo}
-                      className={`img-fluid  rounded-normal  ${
+                      className={`img-fluid rounded-normal ${
                         !props.darkMode ? "d-none" : ""
                       }`}
                       alt="logo"
                     />
                     <img
                       src={logo}
-                      className={`img-fluid  rounded-normal  ${
+                      className={`img-fluid rounded-normal ${
                         props.darkMode ? "d-none" : ""
                       }`}
                       alt="logo"
                     />
                   </div>
                   <h5 className="mb-3 font-weight-bold text-center">
-                    QR Code Generate system{" "}
+                    QR Code Generate System
                   </h5>
                   <p className="text-center text-secondary mb-4">
-                    well come back
+                    Welcome back
                   </p>
                   <div className="mb-5">
                     <p className="line-around text-secondary mb-0">
                       <span className="line-around-1">
-                        {" "}
                         Please use ERP Username and Password
                       </span>
                     </p>
                   </div>
-                  <Form>
+                  <Form onSubmit={handleSubmit}>
                     <Row>
                       <Col lg="12">
                         <Form.Group>
                           <Form.Label className="text-secondary">
-                            User name
+                            Username
                           </Form.Label>
                           <Form.Control
                             type="text"
-                            placeholder="Enter ERP usernam"
+                            placeholder="Enter ERP username"
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
                           />
@@ -123,7 +115,7 @@ const SignIn = (props) => {
                           <Form.Control
                             type="password"
                             placeholder="Enter Password"
-                            valu={password}
+                            value={password}
                             onChange={(e) => setPassword(e.target.value)}
                           />
                         </Form.Group>
@@ -131,13 +123,16 @@ const SignIn = (props) => {
                     </Row>
                     <Button
                       type="submit"
-                      onClick={() => history.push("/")}
                       variant="btn btn-primary btn-block mt-2"
                       style={{ backgroundColor: "#005580" }}
+                      disabled={isLoading}
                     >
-                      Log In
+                      {isLoading ? "Loading..." : "Log In"}
                     </Button>
                   </Form>
+                  {errorMessage && (
+                    <p className="text-danger mt-3">{errorMessage}</p>
+                  )}
                 </Card.Body>
               </Card>
             </Col>
